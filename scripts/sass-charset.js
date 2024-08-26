9c574609b9bbe8d83bdd07b43bc5696f74c6e6ae
raja <raja.rakoto7@gmail.com>
2024-08-26 23:03:40 +0300

    scripts: valid sass-charset.js

--- scripts/sass-charset.js
+++ scripts/sass-charset.js
@@ -2,15 +2,15 @@
  * This script adds the `@charset "UTF-8";` declaration at the beginning of all SCSS files in the project.
  */
 
-const fs = require("node:fs");
-const path = require("node:path");
+import { readdirSync, statSync, readFileSync, writeFileSync } from "node:fs";
+import { join, extname, relative } from "node:path";
 
 // recursively browse files in a directory
 function traverseDirectory(dir) {
-	const files = fs.readdirSync(dir);
+	const files = readdirSync(dir);
 	for (const file of files) {
-		const filePath = path.join(dir, file);
-		const stat = fs.statSync(filePath);
+		const filePath = join(dir, file);
+		const stat = statSync(filePath);
 
 		if (stat.isDirectory()) {
 			// ignore node_modules
@@ -18,7 +18,7 @@ function traverseDirectory(dir) {
 				traverseDirectory(filePath);
 			}
 		} else {
-			if (path.extname(file) === ".scss") {
+			if (extname(file) === ".scss") {
 				processSCSSFile(filePath);
 			}
 		}
@@ -27,24 +27,24 @@ function traverseDirectory(dir) {
 
 // add utf8 at the beginning of an SCSS file
 function processSCSSFile(filePath) {
-	const data = fs.readFileSync(filePath, "utf8");
+	const data = readFileSync(filePath, "utf8");
 	const hasDoubleQuotes = data.startsWith('@charset "UTF-8";');
 	const hasSingleQuotes = data.startsWith("@charset 'UTF-8';");
 
 	if (!hasDoubleQuotes && !hasSingleQuotes) {
 		const newData = `@charset "UTF-8";\n\n${data}`;
-		fs.writeFileSync(filePath, newData);
-		console.log(`${path.relative(process.cwd(), filePath)} encoded ... [done]`);
+		writeFileSync(filePath, newData);
+		console.log(`${relative(process.cwd(), filePath)} encoded ... [done]`);
 	} else {
 		console.log(
-			`${path.relative(process.cwd(), filePath)} already encoded, skipping ...`,
+			`${relative(process.cwd(), filePath)} already encoded, skipping ...`,
 		);
 	}
 }
 
 // entry point
 function startCharset() {
-	const projectRoot = path.join(__dirname, "..");
+	const projectRoot = join(__dirname, "..");
 	traverseDirectory(projectRoot);
 }
 
