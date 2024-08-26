94133de2e7a69dc935da50813f77a6fe1af124db
raja <raja.rakoto7@gmail.com>
2024-08-26 16:52:24 +0300

    eslint: set rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,8 +35,8 @@ module.exports = {
     "prefer-spread": "off",
     "no-case-declarations": "off",
     "no-console": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/no-unused-vars": ["warn"],
     "@typescript-eslint/consistent-type-imports": "warn",
-    "@typescript-eslint/no-unnecessary-condition": "warn",
+    "@typescript-eslint/no-unnecessary-condition": "warn"
   },
 };
